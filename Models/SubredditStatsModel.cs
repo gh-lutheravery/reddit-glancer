82973b5501990599487627065fb07faa8b249cd5
gh-charles <blakos244@gmail.com>
2022-09-25 14:56:50 -0500

    implemented fields

--- Models/SubredditStatsModel.cs
+++ Models/SubredditStatsModel.cs
@@ -1,6 +1,13 @@
-﻿namespace GlanceReddit.Models
+﻿using System.Collections.Generic;
+
+namespace GlanceReddit.Models
 {
 	public class SubredditStatsModel
 	{
+		public Dictionary<string, double> ForeignWebsites { get; set; }
+
+		public Dictionary<string, double> RelatedSubreddits { get; set; }
+
+		public Dictionary<string, double> CrosspostedSubreddits { get; set; }
 	}
 }
