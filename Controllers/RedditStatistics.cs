79625f9f2ef72d4f0306a74608179d3907753ec8
gh-charles <blakos244@gmail.com>
2022-10-05 21:13:56 -0500

    added round on percent

--- Controllers/RedditStatistics.cs
+++ Controllers/RedditStatistics.cs
@@ -197,7 +197,7 @@ namespace GlanceReddit.Controllers
 
 			queryPop.ResultFrequencyBefore = avgDistanceBefore;
 			queryPop.ResultFrequencyNow = avgDistanceNow;
-			queryPop.PercentDifference = (int)((avgDistanceNow - avgDistanceBefore) / avgDistanceBefore * 100);
+			queryPop.PercentDifference = (int)Math.Round(((avgDistanceNow - avgDistanceBefore) / avgDistanceBefore * 100));
 
 			// is the post frequency right now somewhat similar to a month before?
 			if (lesserVariance <= avgDistanceNow && avgDistanceNow <= greaterVariance)
